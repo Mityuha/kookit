a4d815cc1dc61cecdae0e9cd173558544ed1c9c5
mityuha <mit.makaroff@gmail.com>
2023-11-12 21:03:26 +0100

    tmp fix

--- kookit/http_service/requests_diff.py
+++ kookit/http_service/requests_diff.py
@@ -20,7 +20,9 @@ async def compare_requests(
     parsed_url = urlparse(str(request.url))
     parsed_furl = urlparse(str(frequest.url))
 
-    assert parsed_url.path == parsed_furl.path
+    # TODO: Need to check path without params
+    # Because /catalog/{id} != /catalog/2
+    # assert parsed_url.path == parsed_furl.path
 
     if parsed_url.query and parse_qs(parsed_url.query) != parse_qs(parsed_furl.query):
         return f"Expected query params: '{parsed_url.query}', got: '{parsed_furl.query}'"
