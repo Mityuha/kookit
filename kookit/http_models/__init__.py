b225b9d78ced052da5d1ce703be6a3495728c163
mityuha <mit.makaroff@gmail.com>
2023-11-12 19:29:51 +0100

    add xml response

--- kookit/http_models/__init__.py
+++ kookit/http_models/__init__.py
@@ -2,3 +2,4 @@ from .json_request import KookitJSONRequest
 from .json_response import KookitJSONResponse
 from .request import KookitHTTPRequest
 from .response import KookitHTTPResponse
+from .xml_response import KookitXMLResponse
