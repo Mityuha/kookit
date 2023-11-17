5c302a58d7a1b4e28355c16a5427f3df362b1c15
mityuha <mit.makaroff@gmail.com>
2023-11-17 18:01:56 +0100

    requests diff

--- kookit/http_service/requests_diff.py
+++ kookit/http_service/requests_diff.py
@@ -1,5 +1,4 @@
 from typing import Any, Mapping, MutableMapping, Optional, Protocol
-from urllib.parse import parse_qs
 
 from httpx import URL
 
@@ -44,19 +43,14 @@ async def compare_requests(
     if content and content != fcontent:
         return f"Expected body: '{content!r}', got: '{fcontent!r}'"
 
-    # TODO. Maybe, class other than Request required
-    # see tests/http_service/test_diff_headers_and_path_template_response.py
     if request.headers and not all(
         it in frequest.headers.items() for it in request.headers.items()
     ):
         return f"Expected headers present: {dict(request.headers)}, got: {dict(frequest.headers)}"
 
-    # TODO: Need to check path without params
-    # see tests/http_service/test_diff_headers_and_path_template_response.py
-    # Because /catalog/{id} != /catalog/2
     assert request.url.path.format(**frequest.path_params) == frequest.url.path
 
-    if request.url.query and parse_qs(request.url.query) != frequest.url.query:
+    if request.url.query and request.url.query.decode("ascii") != frequest.url.query:
         return f"Expected query params: '{request.url.query!r}', got: '{frequest.url.query}'"
 
     return ""
