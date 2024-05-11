543e2bfbea072f254b7099728ea39da438cc2c26
mityuha <mit.makaroff@gmail.com>
2024-05-11 13:39:12 +0200

    test that won't work

--- kookit/main/client_side.py
+++ kookit/main/client_side.py
@@ -1,36 +1,35 @@
 from typing import Any, Protocol
 
-from httpx import AsyncClient, Response
+from httpx import Client, Response
 
 
 class IKookitService(Protocol):
     @property
-    def service_url(self) -> str:
-        ...
+    def service_url(self) -> str: ...
 
 
 class KookitHTTPAsyncClient:
-    async def request(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        async with AsyncClient(base_url=service.service_url) as client:
-            return await client.request(*args, **kwargs)
+    def request(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        with Client(base_url=service.service_url) as client:
+            return client.request(*args, **kwargs)
 
-    async def get(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "GET", *args, **kwargs)
+    def get(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "GET", *args, **kwargs)
 
-    async def post(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "POST", *args, **kwargs)
+    def post(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "POST", *args, **kwargs)
 
-    async def put(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "PUT", *args, **kwargs)
+    def put(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "PUT", *args, **kwargs)
 
-    async def delete(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "DELETE", *args, **kwargs)
+    def delete(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "DELETE", *args, **kwargs)
 
-    async def options(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "OPTIONS", *args, **kwargs)
+    def options(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "OPTIONS", *args, **kwargs)
 
-    async def patch(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "PATCH", *args, **kwargs)
+    def patch(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "PATCH", *args, **kwargs)
 
-    async def head(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
-        return await self.request(service, "HEAD", *args, **kwargs)
+    def head(self, service: IKookitService, *args: Any, **kwargs: Any) -> Response:
+        return self.request(service, "HEAD", *args, **kwargs)
---
+++ tests/http_service/test_single_http_service_start.py
@@ -0,0 +1,62 @@
+import os
+from typing import Any, Final
+
+import httpx
+import pytest
+from fastapi import APIRouter
+
+from kookit import Kookit, KookitJSONResponse
+
+
+router = APIRouter()
+ROUTER_RESPONSE: Final = [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/users")
+async def read_users():
+    return ROUTER_RESPONSE
+
+
+@pytest.mark.parametrize("with_service", [True, False])
+def test_single_service_response(
+    random_method: str,
+    random_status_code: int,
+    faker: Any,
+    kookit: Kookit,
+    with_service: bool,
+    lifespan_gen: Any,
+) -> None:
+    env_var: str = faker.pystr().upper()
+    resp_json: dict = faker.pydict(value_types=(float, int, str))
+    uri_path: str = f"/{faker.uri_path()}"
+    headers: dict = faker.pydict(value_types=(str,))
+    service = kookit.new_http_service(
+        env_var,
+        unique_url=True,
+        actions=[
+            KookitJSONResponse(
+                resp_json,
+                url=uri_path,
+                method=random_method,
+                status_code=random_status_code,
+                headers=headers,
+            )
+        ],
+        lifespans=[lifespan_gen(), lifespan_gen()],
+        routers=[router],
+    )
+    assert os.environ[env_var] == service.url
+    assert service.name == "service"
+
+    cmanager: Any = service if with_service else kookit
+
+    with cmanager:
+        response = kookit.request(service, random_method, uri_path)
+
+    assert response.status_code == random_status_code
+    assert dict(response.headers).items() >= headers.items()
+    assert response.json() == resp_json
+
+    with cmanager:
+        router_response = httpx.get(f"{service.url}/users")
+    assert router_response.json() == ROUTER_RESPONSE
